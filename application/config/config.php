68db7a3b86261e8e3ebd63d4051ba07f7d134006
widyaluhur <61177655+widyaluhur@users.noreply.github.com>
2021-05-16 20:23:28 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://103.31.250.45:7171/';
+$config['base_url'] = 'http://103.31.250.45:6161/';
 
 /*
 |--------------------------------------------------------------------------
